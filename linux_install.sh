d4b3f0ac6457f133c64b9e1f90795b8315dc8c94
Arghya Sarkar <arghyasarkar.nolan@gmail.com>
2021-08-12 12:56:06 +0530

    Fixed Install Script

--- linux_install.sh
+++ linux_install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 echo "Downloading hydra..."
-curl -L "https://github.com/Shravan-1908/hydra/releases/latest/download/hydra-linux-amd64" -o hydra
+wget https://github.com/Shravan-1908/hydra/releases/latest/download/hydra-linux-amd64 
 
 echo "Adding hydra into PATH..."
 
 mkdir -p ~/.hydra
 
-chmod u+x ./hydra
-
-mv ./hydra ~/.hydra
+mv ./hydra-linux-amd64 ~/.hydra/hydra
+chmod +x ~/.hydra/hydra
 echo "export PATH=$PATH:~/.hydra" >> ~/.bashrc
-set -U fish_user_paths ~/.hydra/ $fish_user_paths      
+fish -c "set -U fish_user_paths ~/.hydra/ $fish_user_paths"      
 echo "export PATH=$PATH:~/.hydra" >> ~/.zshrc
 
 echo "hydra installation is completed!"
